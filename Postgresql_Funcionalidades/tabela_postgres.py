77b13b684309c7972405a656dae9c31063161e8f
pipibodock <fellipe.mirandamatos@gmail.com>
2018-04-16 10:31:52 -0300

    Cria funções para criação de Database.

--- Postgresql_Funcionalidades/tabela_postgres.py
+++ Postgresql_Funcionalidades/tabela_postgres.py
@@ -1,14 +1,35 @@
 import psycopg2
 
 
+def conecta_database():
+    return psycopg2.connect(
+        host='localhost',
+        user='pipibdk',
+        database='basepipi',
+        password='pipizito'
+    )
+
+
+def cria_tabela_database():
+    conn = conecta_database()
+    conn.cursor()
+    cria_tabela = 'CREATE TABLE alunos(matricula SERIAL PRIMARY KEY, nome VARCHAR(80), idade INTEGER)' 
+    cur.execute(cria_tabela) 
+    conn.commit()
+
+
 def teste_de_banco():
-    conn = psycopg2.connect(host = 'localhost', user = 'pipibdk', database = 'basepipi', password = 'pipizito')
+    conn = conecta_database()
     cur = conn.cursor()
-    cur.execute('INSERT INTO nomes (nomes, idade) VALUES (%s, %s)', ('luiz', 20))
+    cur.execute('INSERT INTO nomes (nomes, idade) VALUES (%s, %s)', (_insere_dados()))
     cur.execute('SELECT * FROM nomes')
     cur.fetchall()
-
     conn.commit()
+
+def _insere_dados():
+    nome = input('insira nome: ')
+    idade = int(input('insira idade: '))
+    return nome, idade
 
 
 if __name__ == '__main__':
